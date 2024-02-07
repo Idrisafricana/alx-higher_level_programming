1be488587a2eaa459d625c6a1ff2123808119233
idrisafricana <adeleyedml2@gmail.com>
2024-02-07 01:22:01 +0100

    Pycode fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,7 +12,8 @@ class Rectangle(BaseGeometry):
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
-        """
+            """
+        
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
