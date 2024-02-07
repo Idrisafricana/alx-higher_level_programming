024f7f146be8c317860743bceaa15e0b4428119d
idrisafricana <adeleyedml2@gmail.com>
2024-02-07 04:09:58 -0800

    Fixed pycode error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,10 +10,9 @@ class Rectangle(BaseGeometry):
         """Intialize a new Rectangle.
 
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            """
-        
+        width (int): The width of the new Rectangle.
+        height (int): The height of the new Rectangle.
+        """
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
