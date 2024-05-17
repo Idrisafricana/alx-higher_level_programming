1452c165bf0d3f716b402dc6c6502480a3b9e01f
idrisafricana <adeleyedml2@gmail.com>
2024-05-17 02:17:56 +0100

    Correct code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    url = "https://alx-intranet.hbtn.io/status"
+    request = urllib.request.Request(url)
+    request.add_header('User-Agent', 'Mozilla/5.0')
+    
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
